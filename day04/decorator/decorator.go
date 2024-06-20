c39725b74f9cc5ef1e5ab3993d59133fbeb71767
hejiangda <hejiangda@163.com>
2024-06-20 23:31:26 +0800

    day04

---
+++ day04/decorator/decorator.go
@@ -0,0 +1,36 @@
+package main
+
+// IDraw IDraw
+type IDraw interface {
+	Draw() string
+}
+
+// Square 正方形
+type Square struct{}
+
+// Draw Draw
+func (s Square) Draw() string {
+	return "this is a square"
+}
+
+// ColorSquare 有颜色的正方形
+type ColorSquare struct {
+	square IDraw
+	color  string
+}
+
+// NewColorSquare NewColorSquare
+func NewColorSquare(square IDraw, color string) ColorSquare {
+	return ColorSquare{color: color, square: square}
+}
+
+// Draw Draw
+func (c ColorSquare) Draw() string {
+	return c.square.Draw() + ", color is " + c.color
+}
+
+func main() {
+	square := Square{}
+	colorSquare := NewColorSquare(square, "red")
+	println(colorSquare.Draw())
+}
